13e3ac4a7a4c301b5d5651badfee115375ec59f7
Eldarbai <e.mingazov001@gmail.com>
2016-04-07 00:18:01 +0600

    NavigationDrawer with Fragments

--- app/src/main/java/com/example/batman/toolbarnavigationversion100500/MainActivity.java
+++ app/src/main/java/com/example/batman/toolbarnavigationversion100500/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.batman.toolbarnavigationversion100500;
 
+import android.app.FragmentTransaction;
 import android.content.res.Configuration;
+import android.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,6 +20,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+
+
+
+    private FragmentTransaction fragmentTransaction;
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
@@ -42,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         listView_navigation_drawer = (ListView) findViewById(R.id.navigationList);
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
         listView_navigation_drawer.setAdapter(adapter);
+        listView_navigation_drawer.setOnItemClickListener(new DrawerItemClickListener());
 
 
         setupDrawer();
@@ -52,12 +61,12 @@ public class MainActivity extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close){
           public void onDrawerOpened(View drawerView){
               super.onDrawerOpened(drawerView);
-              getSupportActionBar().setTitle("Navigation");
+              getSupportActionBar().setTitle("Menu");
               invalidateOptionsMenu();
           }
           public void onDrawerClosed(View drawerView){
               super.onDrawerClosed(drawerView);
-              getSupportActionBar().setTitle("mActivityTitle");
+              getSupportActionBar().setTitle("SCRIPTONIT");
               invalidateOptionsMenu();
           }
         };
@@ -86,4 +95,42 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+    private void selectItem(int position){
+        Fragment fragment = null;
+        switch (position){
+            case 0 :
+                fragment = new FragmentNews();
+                break;
+            case 1 :
+                fragment = new FragmentBets();
+                break;
+            case 2 :
+                fragment = new FragmentExperts();
+                break;
+            case 3 :
+                fragment = new FragmentAbout();
+                break;
+            default:
+                break;
+        }
+        if(fragment != null) {
+
+            fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_holder, fragment);
+            fragmentTransaction.commit();
+
+        }else {
+            // Error
+            Log.e(this.getClass().getName(), "Error. Fragment is not created");
+        }
+        listView_navigation_drawer.setItemChecked(position, true);
+        drawerLayout.closeDrawer(listView_navigation_drawer);
+    }
+
 }
