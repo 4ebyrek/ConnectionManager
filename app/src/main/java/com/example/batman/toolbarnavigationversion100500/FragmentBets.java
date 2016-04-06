1bfacb3635dbd1e67a8191219614f24af31f4fff
Eldarbai <e.mingazov001@gmail.com>
2016-04-07 00:32:37 +0600

    NavigationDrawer with Fragments(all)

---
+++ app/src/main/java/com/example/batman/toolbarnavigationversion100500/FragmentBets.java
@@ -0,0 +1,29 @@
+package com.example.batman.toolbarnavigationversion100500;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentBets extends Fragment {
+
+
+    public FragmentBets() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bets, container, false);
+    }
+
+}
